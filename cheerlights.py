a5d5b17b2d55b3ea3e5c909732a649f746903f1b
Jeff Highsmith <jeff@jeffhighsmith.com>
2012-11-25 17:22:18 -0500

    Removed extraneous comments

--- cheerlights.py
+++ cheerlights.py
@@ -4,14 +4,8 @@ def sendCheerlightsColor():
         try:
                 cheerlights = json.loads(s.urlopen('http://api.thingspeak.com/channels/1417/field/1/last.json'))
                 print 'The Cheerlights color was {0} at {1}.'.format(cheerlights['field1'],time.strftime("%H:%M:%S", time.localtime()))
-                #ser = serial.Serial("/dev/tty.usbserial-A900frks", 9600, timeout=1)
                 ser.write(cheerlights['field1'])
                 ser.write(",")
-                #time.sleep(1)
-                #response = ser.readline()
-                #responseColor = response[:len(response)-2]
-                #print 'The tree acknowledges {0}.'.format(responseColor)
-                #ser.close()
         except:
                 print 'Error in sendCheerlightsColor'
 
@@ -25,7 +19,6 @@ if __name__ == '__main__':
 								print 'Re-opening a serial connection'
 								ser = serial.Serial("/dev/tty.usbserial-A900frks", 9600, timeout=1)
 						incoming = ser.readline()
-						#print '|{0}|'.format(incoming,)
 						if incoming[:len(incoming)-2] == '<CheerlightsColor>':
 								print 'Send Color Command Received'
 								sendCheerlightsColor()
